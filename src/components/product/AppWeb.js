55cb5bad025a3236ad809b3fd6ce5733a0b61703
marianteran <marianteranf@gmail.com>
2022-04-17 22:58:40 -0300

    estilos product mediaquery home product

--- src/components/product/AppWeb.js
+++ src/components/product/AppWeb.js
@@ -1,36 +1,53 @@
 import React, { useEffect } from "react";
 import { Link as LinkRouter } from "react-router-dom";
+import appweb from './appweb.png'
+
 const AppWeb = () => {
-   
+
     return (
         <>
 
             <section id="appWeb">
-                <h5>App Web</h5>
+                <div className="container">
+                    <div className='subtitle-principal '>
+                        <h2>Aplicaciones Web</h2>
+                    </div>
+                </div>
 
 
                 <div className="container appwebContainer">
-                    <div className="appWebImage">
-                        <div className="container__vector">
-                            <img src="https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2020/06/Screenshot-5-1024x478.png" alt="" />
-                        </div>
-                    </div>
 
                     <div className="appWebContent">
 
-                        <p>
-                            Una aplicación Web es un conjunto de páginas Web estáticas y dinámicas. Una página Web estática es aquélla que no cambia cuando un usuario la solicita: el servidor Web envía la página al navegador Web solicitante sin modificarla. Por el contrario, el servidor modifica las páginas Web dinámicas antes de enviarlas al navegador solicitante. La naturaleza cambiante de este tipo de página es la que le da el nombre de dinámica.
+                        <h4>
+                            Diseñamos tu sitio para renovar tu imagen y presencia online. </h4>
 
-                            Por ejemplo, podría diseñar una página para que mostrara los resultados del programa de salud y dejara cierta información fuera (como el nombre del empleado y sus resultados) para calcularla cuando la página la solicite un empleado en particular.
+                        <p>
+                            ¿Necesitas generar más ventas? Desarrollamos Sitios Web Institucionales. Landing Pages
+                            Adaptables a todas las pantallas. Diseño de páginas web claras, atractivas y efectivas
+                            que te ayudarán a dar la mejor imagen de tu negocio.
                         </p>
 
-                        <LinkRouter to='/appWeb' className="btn btn-outline-light ms-2">
-                            <button className='btn btn-dark'>Info</button>
-                        </LinkRouter>
+
+
 
 
                     </div>
+                    <div className="appWebImage">
+                        <div className="container__vector">
+                            <img src={appweb} alt="" />
+                        </div>
+                    </div>
+
+
                 </div>
+
+
+                <LinkRouter to='/appWeb' className="container botonPC">
+                    <div className="btnfluorPC">
+                        <a href="">info</a>
+                    </div>
+                </LinkRouter>
 
 
             </section>
