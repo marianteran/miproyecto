37130820d2c5a2e8899ff5251ff90d74fe63b19f
claudiodmguzman <guzmanclaudiodm@gmail.com>
2022-04-20 15:14:05 -0300

    Footer Link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import './footer.css'
+import { Link as Linkrouter } from 'react-router-dom'; 
 
 const Footer = () => {
 
 
-  
+
   return (
     <>
 
-     
-
-        <footer>
 
 
-        <div className="footer">YOURPAGE es un desarrollo integral en conjunto con MINDHUB </div>
-          <div className="footer__copyright">
-            <small> &copy;GRUPO A.</small>
-          </div>
+      <footer className="footer">
 
-        </footer>
-     
+
+        <div className="footer"><b>SEOMA Web Design</b> es un desarrollo integral en conjunto con MINDHUB </div>
+
+        <div className="footer__copyright">
+          <small>&copy;GRUPO A</small>
+        </div>
+
+        <div className='footer_toLink'>
+
+          <div><Linkrouter className="footer__link" to="/">Home</Linkrouter></div>
+
+          <div><Linkrouter className="footer__link" to="/product">Products</Linkrouter></div>
+
+          <div><Linkrouter className="footer__link" to="/servicios">Services</Linkrouter></div>
+
+        </div>
+
+      </footer>
+
 
 
     </>
